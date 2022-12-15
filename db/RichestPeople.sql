cf0fa820498e0a5515bc9fd7050b9c84abeade35
vincent van de merwe <335479@student.mboutrecht.nl>
2022-12-15 14:44:25 +0100

    last commit

---
+++ db/RichestPeople.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 15, 2022 at 01:42 PM
+-- Server version: 5.7.36
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `php-pdo-crud-proeftoets`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `RichestPeople`
+--
+
+DROP TABLE IF EXISTS `RichestPeople`;
+CREATE TABLE IF NOT EXISTS `RichestPeople` (
+  `Id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `Name` varchar(200) NOT NULL,
+  `Networth` varchar(200) NOT NULL,
+  `Age` tinyint(4) NOT NULL,
+  `MyCompany` varchar(200) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `RichestPeople`
+--
+
+INSERT INTO `RichestPeople` (`Id`, `Name`, `Networth`, `Age`, `MyCompany`) VALUES
+(1, 'Elon Musk', '261000000000', 50, 'Tesla'),
+(2, 'Jeff Bezos', '183000000000', 58, 'Amazon'),
+(3, 'Bernard Arnault', '142000000000', 73, 'Louis Vuitton Moët Hennessy'),
+(4, 'Bill Gates', '130000000000', 66, 'Microsoft'),
+(5, 'Warren Buffett', '126000000000', 91, 'Berkshire Hathaway');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
